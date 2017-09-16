25191c967f32acff9a860a28a3ca585b86b0c238
rashadaoud <31595092+rashadaoud@users.noreply.github.com>
2017-09-16 21:59:59 +0200

    rasha.daoud-w2

--- Rasha_Daoud/Week2/Exercise 7.hs
+++ Rasha_Daoud/Week2/Exercise 7.hs
@@ -1,4 +1,4 @@
--- Implementing and testing IBAN validation - time:
+-- Implementing and testing IBAN validation - time: 50 mins
 module Exercise7 where
 
 import Data.List
@@ -10,6 +10,7 @@ import Test.QuickCheck
 
 
 {-http://www.xe.com/ibancalculator/countrylist/-}
+-- Let us consider the support for 4 countries for now
 countryCodesLength = [("NL", 18), ("DE", 22), ("FR", 27), ("ES", 24)]
 
 
@@ -48,19 +49,53 @@ chars2digits cs = [ord (toUpper c) - 55 | c <- move4ToEnd cs, elem (toUpper c) [
 checkSum :: String -> Bool
 checkSum cs = mod (sum $ chars2digits cs) 97 == 1
 
+{- helper, to remove spaces -}
+skipSpaces :: String -> String
+skipSpaces cs = [c | c <- cs, not (ord c == 32)]
+
 iban :: String -> Bool
 iban [] = False
-iban cs = hasIbanMaxLength cs
+iban cs = hasIbanMaxLength cs'
           &&
-          hasValidCountryCode cs
+          hasValidCountryCode cs'
 		  &&
-		  hasValidIbanLengthPerCountry cs
+		  hasValidIbanLengthPerCountry cs'
 		  &&
-		  checkSum cs
+		  checkSum cs'
+		  where cs' = skipSpaces cs
 	
 {- GHCi:
     *Exercise7> iban "NL91ABNA0417164300"
     True
+	*Exercise7> iban "NL91 ABNA 0417 1643 00"
+	True
 -}
 
-{- TODO: optimizing code, comments, testing & testing report -}
+{- Next, test your implementation using some suitable list of examples. -}
+
+ibans :: [(String, Bool)]
+ibans   =       [
+					("NL91ABNA0417164300", True),
+					("NL18ABNA0484869868",True),
+					("ES1020903200500041045040", False),
+					("DE12500105170648489890", False),
+					("FR7630066100410001057380116", False),
+					("NL91ABNA0417164301", True), 
+					("NL18ABNA0484869863", True),
+					("ES10209032005000410450", False),
+					("FR2630066100210001057", False),
+					("DE12500",False)
+				]
+
+testCorrectness :: Bool
+testCorrectness = and [iban (fst c) == snd c| c <- ibans]
+
+main = do
+		print "Test correctness of the validator:"
+		print  testCorrectness
+
+{- GHCi:
+	*Exercise7> main
+	"Test correctness of the validator:"
+	True
+}
