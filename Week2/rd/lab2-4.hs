bcaefccab14aeafb8b3aae56548a62c4bc2f83df
rashadaoud <31595092+rashadaoud@users.noreply.github.com>
2017-09-17 20:40:55 +0200

    1

--- Week2/rd/lab2-4.hs
+++ Week2/rd/lab2-4.hs
@@ -151,6 +151,18 @@ properties = [isReflexive, isCommutativeToItself] --[isCommutative, isAssosiativ
 strengthList :: [Int]
 strengthList = quicksortProbs [0..3]
 
+hasNoUniqueElement :: Eq a => [a] -> [a] -> Bool
+hasNoUniqueElement xs ys = forall xs (\x -> elem x ys) && forall ys (\y -> elem y xs)
+
+
+test :: [Int] -> [Int] -> Bool
+test a b = (hasNoUniqueElement xs ys) --> isPermutation xs ys
+    where
+        xs = nub a
+        ys = nub b
+
+--nub a returns `a` without duplicates
+
 main = do
 print "0)isReflexive      1)isCommutativeToItself:"
 print (compar list1 isReflexive isCommutativeToItself)
@@ -161,3 +173,25 @@ print (compar' list1 isCommutative isAssosiative)
 print (compar2' list1 isCommutative isSymmetric)
 print (compar' list1 isAssosiative isSymmetric)
 print (compar' list1 isAssosiative isCommutative) -}
+
+
+
+{-
+If two sequences are permutations of each other, they have to have the same number of elements, and every element
+of one must be also in the second. Thankfully, due to the requirement that every element is max. once in the sequences,
+we don't have to check how many times is it there, we just need to check if it is present.
+If this weren't the case, we could compare two sequences by sorting each of them and then comparing elements
+at the same index (this is actually faster: O(n log n) vs. O(n^2), however it's more complicated).
+
+We can test these properties:
+a) if the sequences' length is the same
+b) if there is no element unique to one of sequences
+
+if these two are satisfied, then isPermutation should work.
+
+b) is stronger than a), proof by contradiction: let's have two lists without duplicates that satisfy b) but not a).
+every element of a is in b, therefore len(a) <= len(b), also len(b)<=len(a). This is true only if
+the lengths are the same, which is a contradiction.
+-}
+
+
