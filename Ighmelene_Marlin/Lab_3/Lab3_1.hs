2d8db99063020d6dbe3d4c042e7f245e41088a8c
ighmelene <ighmelene@wcw-stud-145-109-38-216.wireless.uva.nl>
2017-09-18 17:13:44 +0200

    Exercise 1

---
+++ Ighmelene_Marlin/Lab_3/Lab3_1.hs
@@ -0,0 +1,25 @@
+module Lab3 where
+
+import Data.List
+import System.Random
+import Test.QuickCheck
+import Lecture3
+
+-- 45 min
+contradiction :: Form -> Bool
+contradiction f = not ( any (\ v -> evl v f) (allVals f) )
+
+-- tautology :: Form -> Bool
+tautology f =  all (\ v -> evl v f) (allVals f)
+
+-- | logical entailment
+entails :: Form -> Form -> Bool
+entails f1 f2 = do 
+                  let f1Trues = filter (\ v -> evl v f1) (allVals f1)
+                  let f2Trues = filter (\ v -> evl v f2) (allVals f2)
+                  f2Trues == [x | x <- f2Trues, elem x f1Trues]
+
+ -- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv f1 f2 = (all (\ v -> evl v f1 == evl v f2) (allVals f1)) && 
+              (all (\ v -> evl v f1 == evl v f2) (allVals f2))
