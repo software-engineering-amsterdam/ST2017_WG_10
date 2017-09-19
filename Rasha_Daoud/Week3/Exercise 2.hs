9d96b7cf757319e43bb3fe25e516615e459c17a3
rashadaoud <31595092+rashadaoud@users.noreply.github.com>
2017-09-19 13:33:28 +0200

    ex2.w3. generator with mulitple depth

--- Rasha_Daoud/Week3/Exercise 2.hs
+++ Rasha_Daoud/Week3/Exercise 2.hs
@@ -34,32 +34,32 @@ getIntL k n = do
 -- 0 means Prop Name, if you go to 1 then it will start using other types
 
 generateForm :: Int -> IO Form
-generateForm 0 =     do
-						n <- getRandomInt 10
-						return (Prop n)
+generateForm 0 = do
+                    n <- getRandomInt 10
+                    return (Prop n)
 
 generateForm level = do
-						n <- getRandomInt 6
-						m <- getRandomInt 10
+                        n <- getRandomInt 6
+                        m <- getRandomInt 10
 						case n of 
-							1 -> do return (Prop m)
-							2 -> do
-									frm <- generateForm (level-1)
-									return (Neg frm) 
-							3 -> do
-									frms <- generateForms m (level-1)
-									return (Dsj frms)
-							4 -> do
-									frms <- generateForms m (level-1)
-									return (Cnj frms)
-							5 -> do
-									frm1 <- generateForm (level-1)
-									frm2 <- generateForm (level-1)
-									return (Impl frm1 frm2) 
-							6 -> do
-									frm1 <- generateForm (level-1)
-									frm2 <- generateForm (level-1)
-									return (Equiv frm1 frm2) 
+                            1 -> do return (Prop m)
+                            2 -> do
+                                    frm <- generateForm (level-1)
+                                    return (Neg frm) 
+                            3 -> do
+                                    frms <- generateForms m (level-1)
+                                    return (Dsj frms)
+                            4 -> do
+                                    frms <- generateForms m (level-1)
+                                    return (Cnj frms)
+                            5 -> do
+                                    frm1 <- generateForm (level-1)
+                                    frm2 <- generateForm (level-1)
+                                    return (Impl frm1 frm2) 
+                            6 -> do
+                                    frm1 <- generateForm (level-1)
+                                    frm2 <- generateForm (level-1)
+                                    return (Equiv frm1 frm2) 
 							--otherwise -> print ("the integer generator is not working as expected...")
 
 generateForms :: Int -> Int -> IO [Form]
@@ -73,17 +73,17 @@ generateForms n l =   do
 testIter :: Int -> (Form -> Bool) -> [Form] -> IO()
 testIter n p [] = print (show n ++ " tests passed...")
 testIter n p (fi:fis) = 
-					if p fi then
-						do
-							print ("test passed on:" ++ show fi)
-							testIter n p fis
-					else
-						error ("test failed on:" ++ show fi)
+                        if p fi then
+                         do
+                          print ("test passed on:" ++ show fi)
+                          testIter n p fis
+                        else
+                          error ("test failed on:" ++ show fi)
   
 testForms :: Int -> Int -> (Form -> Bool)-> IO()
 testForms n l p = do 
-					forms <- generateForms n l
-					testIter n p forms
+                      forms <- generateForms n l
+                      testIter n p forms
 
 testParse :: Int -> IO()
 testParse level = testForms 100 level (\str -> let [forms] = parse(show str) in show str == show forms)
@@ -98,6 +98,6 @@ instance Show Form where
 
 
 main = do
-		testParse 0
-		testParse 1
-		testParse 2
+          testParse 0
+          testParse 1
+          testParse 2
