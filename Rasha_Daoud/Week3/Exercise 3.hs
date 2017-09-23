f9d03d34d2f6719013ea1c8ebce10e56d4c91ade
rashadaoud <31595092+rashadaoud@users.noreply.github.com>
2017-09-23 16:44:44 +0200

    rasha.daoud-w3

--- Rasha_Daoud/Week3/Exercise 3.hs
+++ Rasha_Daoud/Week3/Exercise 3.hs
@@ -17,7 +17,7 @@ cnf (Dsj frms)     = if (length frms) == 0 then (Dsj []) else if (length frms) =
 cnf (Cnj frms)     = Cnj (map cnf frms)
 cnf _              = Cnj []
 
--- morgan law
+-- morgan law needed for the conversion
 applyMorganLaw :: Form -> Form -> Form
 applyMorganLaw frm frm'                     = Cnj [frm, frm']
 applyMorganLaw (Cnj (frm:frms)) frm'        = Cnj [applyMorganLaw frm frm', applyMorganLaw (Cnj frms) frm']
@@ -28,9 +28,9 @@ convert2CNF :: Form -> Form
 convert2CNF form = cnf $ nnf $ arrowfree form -- lecture 3 slides
 
 {---------------------------------------------------Testing-----------------------------------------------------------}
-{- TODO -}
 
-{- GHCi:
+{- We can pass any form to the conversion function.
+    GHCi:
 	convert2CNF (Dsj [Neg (Prop 3), Impl (Prop 1) (Prop 4)])
 	*(-3 +((1==>4)))
 	
@@ -38,5 +38,24 @@ convert2CNF form = cnf $ nnf $ arrowfree form -- lecture 3 slides
     *(-3 +(+(1 +(3 4))))
 	
 	convert2CNF (Dsj [Impl (Prop 1) (Prop 2), Neg (Prop 3)])
-    *(*(-1 +(2)) +(-3)
+    *(*(-1 +(2)) +(-3))
+
+ Furthermore, we can test wether the conversion went fine, if the properties of the original statement still holds with the converted one.
+ ** Let's take the first example: (Dsj [Neg (Prop 3), Impl (Prop 1) (Prop 4)])
+      converting it to CNF gave:
+	      *(-3 +((1==>4))) which is equivalent to: 
+               Cnj [Dsj [(Neg (Prop1)), Impl (Prop 1) (Prop4)]]
+
+    - original form  
+	  *Exercise1> tautology (Dsj [Neg (Prop 3), Impl (Prop 1) (Prop 4)])
+      False
+      *Exercise1> satisfiable (Dsj [Neg (Prop 3), Impl (Prop 1) (Prop 4)])
+      True
+
+    -converted form
+      *Exercise1> tautology (Cnj [Dsj [(Neg (Prop 1)), Impl (Prop 1) (Prop 4)]])
+      False
+      *Exercise1> satisfiable (Cnj [Dsj [(Neg (Prop 1)), Impl (Prop 1) (Prop 4)]])
+      True
+
 -}
